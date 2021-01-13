a59e39e3d1e800fad2c0f2438626a6d2fd655238
michaelrugnetta <mg.rugnetta@gmail.com>
2021-01-13 10:22:32 -0800

    feat: Add print line for project creator

--- bikeshare.py
+++ bikeshare.py
@@ -15,6 +15,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    print('Created by Mike')
     print('Hello! Let\'s explore some US bikeshare data!\n')
     city = ''
     print('Choose which city to filter by: Chicago, New York City, Washington\n')
