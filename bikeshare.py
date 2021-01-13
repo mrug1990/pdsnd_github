7312e81d3e941dfcdf14d6598d7359e4620c18e6
michaelrugnetta <mg.rugnetta@gmail.com>
2021-01-13 10:24:54 -0800

    style: Removed some code function description in get_filters()

--- bikeshare.py
+++ bikeshare.py
@@ -10,10 +10,6 @@ def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
 
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Created by Mike')
     print('Hello! Let\'s explore some US bikeshare data!\n')
